4e172587d67dc345c9bdd197facbf9736aabeb53
Otavio Henrique <otaviooliveirapt@hotmail.com>
2026-06-01 10:38:08 -0300

    feat: criação da classe abstrata Pessoa

---
+++ nutrifit/src/main/java/br/com/nutrifit/model/Pessoa.java
@@ -0,0 +1,31 @@
+package br.com.nutrifit.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O nome é obrigatório")
+    private String nome;
+
+    @Email(message = "Email inválido")
+    @NotBlank(message = "O email é obrigatório")
+    private String email;
+
+    @NotBlank(message = "A senha é obrigatória")
+    private String senha;
+
+}
