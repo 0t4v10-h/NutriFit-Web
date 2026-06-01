89fec969810d496657909f0756e0511a38bdba72
Otavio Henrique <otaviooliveirapt@hotmail.com>
2026-06-01 10:13:48 -0300

    feat: criação da arquitetura inicial do projeto

---
+++ nutrifit/src/main/java/br/com/nutrifit/NutrifitApplication.java
@@ -0,0 +1,13 @@
+package br.com.nutrifit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrifitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutrifitApplication.class, args);
+	}
+
+}
---
+++ nutrifit/src/test/java/br/com/nutrifit/NutrifitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nutrifit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrifitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
