29cd1cf6775989114975d5737a1d7e14d1c3e25f
Otavio Henrique <otaviooliveirapt@hotmail.com>
2026-06-17 09:10:07 -0300

    fix: adicionar validacao de campos obrigatorios no agendamento (data, horario e status)

--- nutrifit/src/main/java/br/com/nutrifit/model/Agendamento.java
+++ nutrifit/src/main/java/br/com/nutrifit/model/Agendamento.java
@@ -2,6 +2,8 @@ package br.com.nutrifit.model;
 
 import br.com.nutrifit.model.enums.StatusAgendamento;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,10 +20,14 @@ public class Agendamento {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message = "A data do agendamento é obrigatória")
+    @FutureOrPresent(message = "A data não pode estar no passado")
     private LocalDate data;
 
+    @NotNull(message = "O horário do agendamento é obrigatório")
     private LocalTime horario;
 
+    @NotNull(message = "Selecione o status do agendamento")
     @Enumerated(EnumType.STRING)
     private StatusAgendamento status;
 
